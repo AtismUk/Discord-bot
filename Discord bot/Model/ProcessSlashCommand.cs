91233caeee3160ca6cb634577eb128f468424016
Антон Виншу <fangun133@gmail.com>
2023-02-06 15:53:11 +0300

    Custom command

--- Discord bot/Model/ProcessSlashCommand.cs
+++ Discord bot/Model/ProcessSlashCommand.cs
@@ -19,43 +19,72 @@ namespace Discord_bot.Model
 
         public async Task GetAllUsersDidntReaction(SocketSlashCommand command)
         {
+            // Получаем все нужные данные
             var channel = (SocketTextChannel)command.Data.Options.First().Value;
             var number = command.Data.Options.ElementAt(1).Value.ToString();
-            var role = command.Data.Options.Last().Name.ToString() != "number" ? command.Data.Options.Last().Value.ToString(): null;
+            var role = command.Data.Options.ElementAt(2).Value.ToString();
+            var roleOptionally = command.Data.Options.Last().Name.ToString() != "Optionally role" ? command.Data.Options.Last().Value.ToString(): null;
 
 
-            //Получаем все сообщения и достае т от туда нам нужное
+            //Получаем все сообщения и достаем от туда нужное
             var messages = channel.GetMessagesAsync().FlattenAsync();
             var message = messages.Result.ElementAt(int.Parse(number) - 1);
-
-            var usersAll = _client.GetGuild((ulong)command.GuildId).Users.ToList();
-
-            List<IUser> usersList = new List<IUser>();
-            List<IUser> userReactions = new();
-            foreach (var reactionEmoji in message.Reactions.Keys)
+            //Строка с юзерами, которые не отреагировали на сообщение
+            string resultUsers = "Выбранное вами сообщение не содержит реакций";
+            if (message.Reactions.Count() > 0)
             {
-                var reactions = message.GetReactionUsersAsync(new Emoji(reactionEmoji.Name), 100).FlattenAsync();
-                userReactions.AddRange(reactions.Result);
-            }
-            //Все люди, которые поставили реакцию
-            var notRepeat = userReactions.DistinctBy(x => x.Id).ToList();
-            foreach (var user in notRepeat)
-            {
-                if (!notRepeat.Select(x => x.Id).Contains(user.Id))
+                //Подучаем всех пользователей с учетом того, что мы отсеиваем пользователей с выбранной ролью
+                List<IUser> users = new List<IUser>();
+                foreach (var user in _client.GetGuild((ulong)command.GuildId).Users.ToList().Where(x => x.IsBot == false))
                 {
-                    usersList.Add(_client.GetUser(user.Id));
+                    if (roleOptionally != null)
+                    {
+                        if (!user.Roles.Select(x => x.Name).Contains(role) && !user.Roles.Select(x => x.Name).Contains(roleOptionally))
+                        {
+                            users.Add(user);
+                        }
+                    }
+                    else
+                    {
+                        if (!user.Roles.Select(x => x.Name).Contains(role))
+                        {
+                            users.Add(user);
+                        }
+                    }
+                }
+
+                //Избегаем повторений, создаем окончательный список людей, которые не поставили ни одной реакции.
+                List<IUser> usersList = new List<IUser>();
+                List<IUser> userReactions = new();
+                //Идем в сообщения, вытаскиваем всех пользаков, котоые поставили рекакцию
+                foreach (var reactionEmoji in message.Reactions.Keys)
+                {
+                    var reactions = message.GetReactionUsersAsync(new Emoji(reactionEmoji.Name), 100).FlattenAsync();
+                    userReactions.AddRange(reactions.Result);
+                }
+                //Теперь убераем повторения
+                var notRepeat = userReactions.DistinctBy(x => x.Id).ToList();
+                //Записываем людей, которые не поставили реакцию
+                foreach (var user in users)
+                {
+                    if (!notRepeat.Select(x => x.Id).Contains(user.Id))
+                    {
+                        usersList.Add(_client.GetUser(user.Id));
+                    }
+                }
+
+                resultUsers = "";
+                foreach (var item in usersList)
+                {
+                    resultUsers += "\n" + item.Mention + " -> " + item.Username;
                 }
             }
 
 
-            string properUsers = "";
-            foreach (var item in usersList)
-            {
-                properUsers += "\n" + item.Mention;
-            }
             var embedBuilder = new EmbedBuilder()
-                .WithTitle("Reactions")
-                .WithDescription(properUsers)
+                .WithTitle("Люди, которые не проголосовали")
+                .WithDescription(resultUsers + "\n" + $@"Канал сообщения: <#{message.Channel.Id}>")
+                .WithFooter("Сообщение под номером: " + number)
                 .WithCurrentTimestamp();
 
             await command.RespondAsync(embed: embedBuilder.Build());
--- Discord bot/Program.cs
+++ Discord bot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.Net;
 using Discord.WebSocket;
+using Discord_bot.Model;
 using Newtonsoft.Json;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
@@ -13,7 +14,8 @@ namespace Discord_bot
 
         DiscordSocketClient _client = new(new()
         {
-            GatewayIntents = GatewayIntents.All
+            GatewayIntents = GatewayIntents.All,
+            UseInteractionSnowflakeDate = false
         });
 
         static Task Main(string[] args) => new Program().MainAsync();
@@ -21,6 +23,7 @@ namespace Discord_bot
         async Task MainAsync()
         {
             _client.Log += Log;
+            _client.Ready += Client_Ready;
             _client.SlashCommandExecuted += SlashCommandHandler;
 
             var token = "MTA2OTk3NjQ3MTkzNTkxNDA0Ng.G_NkGV.efHPdkfb5MXGFL3Ejj5uaY2IQSZ8Q-U6Be-7_M";
@@ -33,56 +36,14 @@ namespace Discord_bot
 
         private async Task SlashCommandHandler(SocketSlashCommand command)
         {
+            ProcessSlashCommand slashCommand = new(_client);
             switch (command.Data.Name)
             {
                 case "get-all-users-didnt-vote":
-                    await GetAllUsersDidntVote(command);
+                    await slashCommand.GetAllUsersDidntReaction(command);
                     break;
             }
         }
-
-        private async Task GetAllUsersDidntVote(SocketSlashCommand command)
-        {
-            var number = command.Data.Options.Last().Value.ToString();
-            var channel = (SocketTextChannel)command.Data.Options.First().Value;
-
-            //Получаем все сообщения и достае т от туда нам нужное
-            var messages = channel.GetMessagesAsync().FlattenAsync();
-            var message = messages.Result.ElementAt(int.Parse(number)-1);
-
-            var users = _client.GetGuild((ulong)command.GuildId).Users;
-            List<IUser> usersList = new List<IUser>();
-            List<IUser> userReactions = new();
-            foreach (var reactionEmoji in message.Reactions.Keys)
-            {
-                var reactions = message.GetReactionUsersAsync(new Emoji(reactionEmoji.Name), 100).FlattenAsync();
-                userReactions.AddRange(reactions.Result);
-            }
-            //Все люди, которые поставили реакцию
-            var notRepeat = userReactions.DistinctBy(x => x.Id).ToList();
-            foreach (var user in users)
-            {
-                if (!notRepeat.Select(x => x.Id).Contains(user.Id))
-                {
-                    usersList.Add(_client.GetUser(user.Id));
-                }
-            }
-
-
-            string properUsers = "";
-            foreach (var item in usersList)
-            {
-                properUsers += "\n" + item.Mention;
-            }
-            var embedBuilder = new EmbedBuilder()
-                .WithTitle("Reactions")
-                .WithDescription(properUsers)
-                .WithColor(Color.Green)
-                .WithCurrentTimestamp();
-
-            await command.RespondAsync(embed: embedBuilder.Build());
-        }
-
         private async Task Client_Ready()
         {
 
@@ -90,6 +51,9 @@ namespace Discord_bot
             globalCommand.WithName("get-all-users-didnt-vote");
             globalCommand.WithDescription("Получить всех, кто не голосовал");
             globalCommand.AddOption("channel", ApplicationCommandOptionType.Channel, "Канал с голосованием", isRequired: true);
+            globalCommand.AddOption("number", ApplicationCommandOptionType.Number, "Номер сообщения", isRequired: true);
+            globalCommand.AddOption("role", ApplicationCommandOptionType.Role, "Роль, которую не учитываем", isRequired: true);
+            globalCommand.AddOption("role_optional", ApplicationCommandOptionType.Role, "Дополнительная роль", isRequired: false);
 
             try
             {
