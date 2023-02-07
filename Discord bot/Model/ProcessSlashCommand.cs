a65caefa0353781c7f801d8118647ebe2072b243
Антон Виншу <fangun133@gmail.com>
2023-02-07 11:43:40 +0300

    Fix bug

--- Discord bot/Model/ProcessSlashCommand.cs
+++ Discord bot/Model/ProcessSlashCommand.cs
@@ -19,26 +19,32 @@ namespace Discord_bot.Model
 
         public async Task GetAllUsersDidntReaction(SocketSlashCommand command)
         {
-            // Получаем все нужные данные
-            var channel = (SocketTextChannel)command.Data.Options.First().Value;
-            var number = command.Data.Options.ElementAt(1).Value.ToString();
-            var role = command.Data.Options.ElementAt(2).Value.ToString();
-            var roleOptionally = command.Data.Options.Last().Name.ToString() != "Optionally role" ? command.Data.Options.Last().Value.ToString(): null;
-
-
-            //Получаем все сообщения и достаем от туда нужное
-            var messages = channel.GetMessagesAsync().FlattenAsync();
-            var message = messages.Result.ElementAt(int.Parse(number) - 1);
-            //Строка с юзерами, которые не отреагировали на сообщение
             string resultUsers = "Выбранное вами сообщение не содержит реакций";
-            if (message.Reactions.Count() > 0)
+            var embedBuilder = new EmbedBuilder();
+            try
             {
+                // Получаем все нужные данные
+                var channel = (SocketTextChannel)command.Data.Options.First().Value;
+                var number = command.Data.Options.ElementAt(1).Value.ToString();
+                var role = command.Data.Options.ElementAt(2).Value.ToString();
+                var roleOptionally = command.Data.Options.Last().Name.ToString() != "Optionally role" ? command.Data.Options.Last().Value.ToString() : null;
+
+
+                //Получаем все сообщения и достаем от туда нужное
+                var messages = channel.GetMessagesAsync().FlattenAsync();
+                var message = messages.Result.ElementAt(int.Parse(number) - 1);
+                //Строка с юзерами, которые не отреагировали на сообщение
+                if (message.Reactions.Count() < 1)
+                {
+                    throw new Exception("Под сообщением нет ни одной реакции");
+                }
                 //Подучаем всех пользователей с учетом того, что мы отсеиваем пользователей с выбранной ролью
                 List<IUser> users = new List<IUser>();
-                foreach (var user in _client.GetGuild((ulong)command.GuildId).Users.ToList().Where(x => x.IsBot == false))
+                foreach (var user in channel.Users.ToList().Where(x => x.IsBot == false))
                 {
                     if (roleOptionally != null)
                     {
+                        //Проверяем, чтобы у рользока не было роли, которую мы исключили, а так же доступа к каналу, где проводилось голосование, ибо зачем его выводить как не голсовавшего, если он и так не мог проголосовать.
                         if (!user.Roles.Select(x => x.Name).Contains(role) && !user.Roles.Select(x => x.Name).Contains(roleOptionally))
                         {
                             users.Add(user);
@@ -46,7 +52,7 @@ namespace Discord_bot.Model
                     }
                     else
                     {
-                        if (!user.Roles.Select(x => x.Name).Contains(role))
+                        if (!user.Roles.Select(x => x.Name).Contains(role) && channel.Users.Contains(user))
                         {
                             users.Add(user);
                         }
@@ -72,20 +78,25 @@ namespace Discord_bot.Model
                         usersList.Add(_client.GetUser(user.Id));
                     }
                 }
-
                 resultUsers = "";
                 foreach (var item in usersList)
                 {
                     resultUsers += "\n" + item.Mention + " -> " + item.Username;
                 }
+                embedBuilder.WithTitle("Люди, которые не проголосовали");
+                embedBuilder.WithDescription(resultUsers + "\n" + $@"Канал сообщения: <#{message.Channel.Id}>");
+                embedBuilder.WithFooter("Сообщение под номером: " + number);
+                embedBuilder.WithCurrentTimestamp();
+            }
+            catch (Exception ex)
+            {
+                resultUsers = ex.Message.ToString();
+                embedBuilder.WithTitle("Ошибка");
+                embedBuilder.WithDescription(resultUsers);
+                embedBuilder.WithCurrentTimestamp();
             }
 
-
-            var embedBuilder = new EmbedBuilder()
-                .WithTitle("Люди, которые не проголосовали")
-                .WithDescription(resultUsers + "\n" + $@"Канал сообщения: <#{message.Channel.Id}>")
-                .WithFooter("Сообщение под номером: " + number)
-                .WithCurrentTimestamp();
+            
 
             await command.RespondAsync(embed: embedBuilder.Build());
         }
